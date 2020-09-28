94096c434ffb5c88e80e74116b1f937e3c2349ec
Charles Machakwa <69157659+CharlesTMac@users.noreply.github.com>
2020-09-28 14:12:07 +0200

    Add files via upload

---
+++ precache-manifest.77b6e183a37bbe0f1ef7235aedad4547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769a14a408ae38d0e4a6f4f3f13293cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c170fe5ddd8f195b6bb0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f560f89af117390c2d13",
+    "url": "/static/js/2.a25067c4.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.a25067c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c170fe5ddd8f195b6bb0",
+    "url": "/static/js/main.92637860.chunk.js"
+  },
+  {
+    "revision": "e5a3a9296d8a9471c688",
+    "url": "/static/js/runtime-main.73564122.js"
+  }
+]);
